0f85e735f6da7dded5f34373d57b54c252ff18e6
ZeluLi <lizeusdut@qq.com>
2016-09-11 19:51:41 +0800

    update source code

---
+++ ListDataStruct/ListDataStruct/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  ListDataStruct
+//
+//  Created by ZeluLi on 16/9/11.
+//  Copyright © 2016年 zeluli. All rights reserved.
+//
+
+import Foundation
+
+let array = "a,b,c,d,e,f,g".componentsSeparatedByString(",")
+
+let sequcenceList: SequenceList = SequenceList(capacity: 10)
+
+for item in array {
+    sequcenceList.addItem(item)
+}
+sequcenceList.displayList()
+
+sequcenceList.insert("z", index: 0)
+
+sequcenceList.displayList()
