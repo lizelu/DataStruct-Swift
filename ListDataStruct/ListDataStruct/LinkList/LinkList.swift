2949ce1c034ad25eec95c44c3b8c8c84d4c4f85c
zeluliCredit <zeluli@creditease.cn>
2016-09-14 15:24:44 +0800

    udpate source code

--- ListDataStruct/ListDataStruct/LinkList/LinkList.swift
+++ ListDataStruct/ListDataStruct/LinkList/LinkList.swift
@@ -8,14 +8,87 @@
 
 import Foundation
 class OneDirectionLinkListNote {
+    var data: String
     var next: OneDirectionLinkListNote?
-    var data: String = ""
     
-    init(data: String) {
+    init(data: String = "") {
         self.data = data
+    }
+    
+    deinit{
+        print("\(self.data)-节点释放")
     }
 }
 
 class OneDirectionLinkList {
+    var headNote: OneDirectionLinkListNote?
+    var tailNote: OneDirectionLinkListNote?
+    var length: UInt
+    
+    init() {
+        self.headNote = OneDirectionLinkListNote()
+        self.tailNote = self.headNote
+        self.length = 0
+    }
+    
+    func forwardDirectionCreateList(items: Array<String>) -> Bool {
+        for item in items {
+            let newLinkListNote = OneDirectionLinkListNote(data: item)
+            if self.tailNote == nil {
+                return false
+            }
+            self.tailNote?.next = newLinkListNote
+            self.tailNote = newLinkListNote
+            self.length += 1
+        }
+        return true
+    }
+    
+    func reverseDirectionCreateList(items: Array<String>) -> Bool {
+        for item in items {
+            let newLinkListNote = OneDirectionLinkListNote(data: item)
+            if self.headNote == nil {
+                return false
+            }
+            newLinkListNote.next = headNote?.next
+            self.headNote?.next = newLinkListNote
+            self.length += 1
+            if length == 1 {
+                self.tailNote = newLinkListNote;
+            }
+        }
+        return true
+    }
+    
+    func removeAllItem() {
+        guard var removeItem: OneDirectionLinkListNote = self.headNote?.next else {
+            return
+        }
+        while self.length != 0 {
+            self.headNote?.next = removeItem.next
+            removeItem.next = nil
+            self.length -= 1
+            if self.headNote?.next != nil {
+                removeItem = (self.headNote?.next)!
+            } else {
+                self.tailNote = self.headNote
+                break
+            }
+        }
+    }
+    
+    func display() {
+        var currentNote = self.headNote?.next
+        for _ in 0..<self.length {
+            if currentNote == nil {
+                break
+            }
+            guard let item = currentNote?.data else {
+                return
+            }
+            print(item)
+            currentNote = currentNote?.next
+        }
+    }
     
 }
--- ListDataStruct/ListDataStruct/main.swift
+++ ListDataStruct/ListDataStruct/main.swift
@@ -10,15 +10,23 @@ import Foundation
 
 let array = "a,b,c,d,e,f,g".componentsSeparatedByString(",")
 
-let sequcenceList: SequenceList = SequenceList(capacity: 10)
+//let sequcenceList: SequenceList = SequenceList(capacity: 10)
+//
+//for item in array {
+//    sequcenceList.addItem(item)
+//}
+//sequcenceList.displayList()
+//
+//sequcenceList.insert("z", index: 0)
+//sequcenceList.removeItme(1)
+//sequcenceList.modify(3, newItem: "m")
+//
+//sequcenceList.displayList()
 
-for item in array {
-    sequcenceList.addItem(item)
-}
-sequcenceList.displayList()
-
-sequcenceList.insert("z", index: 0)
-sequcenceList.removeItme(1)
-sequcenceList.modify(3, newItem: "m")
-
-sequcenceList.displayList()
+let oneDirectionLinkList = OneDirectionLinkList()
+oneDirectionLinkList.forwardDirectionCreateList(array)
+oneDirectionLinkList.display()
+oneDirectionLinkList.removeAllItem()
+oneDirectionLinkList.reverseDirectionCreateList(array)
+oneDirectionLinkList.display()
+        
