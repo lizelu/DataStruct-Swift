2f6d76a4de1e62591592035bba28762679e7a600
zeluliCredit <zeluli@creditease.cn>
2016-09-14 14:11:31 +0800

    update source code

---
+++ ListDataStruct/ListDataStruct/LinkList/LinkList.swift
@@ -0,0 +1,21 @@
+//
+//  LinkList.swift
+//  ListDataStruct
+//
+//  Created by Mr.LuDashi on 16/9/14.
+//  Copyright © 2016年 zeluli. All rights reserved.
+//
+
+import Foundation
+class OneDirectionLinkListNote {
+    var next: OneDirectionLinkListNote?
+    var data: String = ""
+    
+    init(data: String) {
+        self.data = data
+    }
+}
+
+class OneDirectionLinkList {
+    
+}
