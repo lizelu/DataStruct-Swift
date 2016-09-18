95c7ac6237acd747c782990405386f4220392be2
zeluliCredit <zeluli@creditease.cn>
2016-09-18 10:32:39 +0800

    update queue source code

--- StackAndQueue/StackAndQueue/Link/LinkQueue.swift
+++ StackAndQueue/StackAndQueue/Link/LinkQueue.swift
@@ -1,5 +1,5 @@
 //
-//  QueueProtocal.swift
+//  LinkQueue.swift
 //  StackAndQueue
 //
 //  Created by Mr.LuDashi on 16/9/18.
@@ -8,53 +8,71 @@
 
 import Foundation
 
-protocol QueueType {
+class LinkQueue: QueueType {
+    
+    // MARK: - QueueType
+    
     /**
      获取队头结点值
      
      - returns: 头结点
      */
-    func getHead() -> AnyObject?
+    func getHead() -> AnyObject? {
+        return nil
+    }
     
     /**
      进入队列
      
      - returns: 空
      */
-    func enQueue() -> Void
+    func enQueue() -> Void {
+        
+    }
     
     /**
      获取尾结点值
      
      - returns: 返回队尾的结点值
      */
-    func getTail() -> AnyObject?
+    func getTail() -> AnyObject? {
+        return nil
+    }
     
     /**
      出队列
      
      - returns: 出队列的值
      */
-    func deQueue() -> AnyObject?
+    func deQueue() -> AnyObject? {
+        return nil
+    }
     
     /**
      队列的长度
      
      - returns: 队列中元素的个数
      */
-    func queueLength() -> Int
+    func queueLength() -> Int {
+        return 0
+    }
     
     /**
      判断队列是否为空
      
      - returns: true - 空，  false - 不为空
      */
-    func queueIsEmpty() -> Bool
+    func queueIsEmpty() -> Bool {
+        return true
+    }
     
     /**
      遍历输出队列中的值
      
      - returns:
      */
-    func display() -> Void
+    func display() -> Void {
+        
+    }
+
 }
