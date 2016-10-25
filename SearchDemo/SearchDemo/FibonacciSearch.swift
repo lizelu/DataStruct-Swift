ef1edda976d7932b52b7112a246348f6aa2a82b4
zeluliCredit <zeluli@creditease.cn>
2016-10-25 16:47:15 +0800

    update source code about fibonacci search

--- SearchDemo/SearchDemo/FibonacciSearch.swift
+++ SearchDemo/SearchDemo/FibonacciSearch.swift
@@ -14,21 +14,34 @@ class FibonacciSearch: SearchType {
     /// - returns: 返回创建好的Fibonacci数列
     private func createFibonacciSequence() -> Array<Int> {
         var fibonacciSequence = [0, 1];
-        for i in 2..<30 {
+        for i in 2..<12 {
             fibonacciSequence.append(fibonacciSequence[i-1] + fibonacciSequence[i-2])
         }
         return fibonacciSequence
     }
-    func search(items: Array<Int>, item: Int) -> Int {
-        
-        //寻找元素的个数在Fibonacci数列中对应区间的位置
+    
+    
+    /// 寻找number在Fibonacci数列中的位置
+    ///
+    /// - parameter number:
+    ///
+    /// - returns: 位置索引
+    private func findNumberInFibonacci(number: Int) -> Int {
         let fibonacciSequence = createFibonacciSequence()
-        var key = 0
-        while items.count >= fibonacciSequence[key] {
-            key += 1
+        print("Fibonacci数列：\(fibonacciSequence)")
+        var index = 0
+        while number >= fibonacciSequence[index] {
+            index += 1
         }
+        return index
+    }
+    
+    func search(items: Array<Int>, item: Int) -> Int {
+        //寻找元素的个数在Fibonacci数列中对应区间的位置
+        var key = findNumberInFibonacci(number: items.count)
         
-        //将数组元素补齐，也就是让searchItems.count = fibonacciSequence[key]
+        //查找表的元素补齐，便于使用Fibonacci数列进行分割
+        let fibonacciSequence = createFibonacciSequence()
         var searchItems = items
         for _ in 0..<(fibonacciSequence[key]-items.count) {
             searchItems.append(items.last!)
@@ -42,9 +55,6 @@ class FibonacciSearch: SearchType {
             //前半部分元素的个数为F(key - 1)
             //那么后半部分元素的个数自然就为F(key - 2)
             let mid = low + fibonacciSequence[key - 1] - 1
-            
-//            print("mid(\(mid + 1)) = \(searchItems[mid])")
-            
             if item < searchItems[mid] {
                 high = mid - 1      //改变high的位置，查找范围的个数为F(key - 1)：前半部分的个数
                 key = key - 1       //因为查找范围的个数为F(key - 1)，所以更新key的值为key-1
